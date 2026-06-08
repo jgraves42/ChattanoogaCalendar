24dd4d0b2528c304939c8eae60432d66e7eb676b
jgraves42 <133706711+jgraves42@users.noreply.github.com>
2026-06-08 09:48:51 +0000

    chore: weekly event data update [skip ci]

--- data/events.js
+++ data/events.js
@@ -1,5 +1,5 @@
 window.CHATTANOOGA_EVENTS = {
-  "last_updated": "2026-06-01T10:02:13.623586+00:00",
+  "last_updated": "2026-06-08T09:48:51.361676+00:00",
   "events": [
     {
       "id": "c342b4f29f01",
@@ -11,6 +11,28 @@ window.CHATTANOOGA_EVENTS = {
       "venue": "Tennessee Aquarium",
       "category": "Family",
       "source": "tnaqua.org"
+    },
+    {
+      "id": "6d1b99a8a5a5",
+      "title": "Ice Cream Safari",
+      "start": "2026-06-13T16:00:00",
+      "end": null,
+      "description": "Visit your favorite animal friends while tasting sweet treats from Ben & Jerry's, Blue Bell, Mayfield, Dippin' Dots, The Ice Cream Show and more. You can also watch our animals join in on the fun with their own special frozen treats!",
+      "url": "https://www.chattzoo.org/events/ice-cream-safari",
+      "venue": "Chattanooga Zoo",
+      "category": "Food",
+      "source": "chattzoo.org"
+    },
+    {
+      "id": "5d19e2be3b51",
+      "title": "Q 'n Brew",
+      "start": "2026-07-11T18:00:00",
+      "end": null,
+      "description": "Enjoy barbeque and other summertime classics from great local restaurants at this family-friendly cookout. Refreshing beer and a fabulous bluegrass band will keep everyone rocking! Plus, enjoy up-close animal meet and greets, fun lawn games and thrilling tortoise races!",
+      "url": "https://www.chattzoo.org/events/q-n-brew",
+      "venue": "Chattanooga Zoo",
+      "category": "Music",
+      "source": "chattzoo.org"
     },
     {
       "id": "809556b65fda",
