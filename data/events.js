3b26fa55b618180e0749034ed52319a109dcc075
jgraves42 <133706711+jgraves42@users.noreply.github.com>
2026-08-17 08:53:05 +0000

    chore: weekly event data update [skip ci]

--- data/events.js
+++ data/events.js
@@ -1,5 +1,5 @@
 window.CHATTANOOGA_EVENTS = {
-  "last_updated": "2026-08-10T09:41:09.480064+00:00",
+  "last_updated": "2026-08-17T08:53:05.048855+00:00",
   "events": [
     {
       "id": "3427bad7d9ac",
@@ -19,6 +19,17 @@ window.CHATTANOOGA_EVENTS = {
       "end": null,
       "description": "Animal lovers who are 65+, this is your day! Seniors receive FREE Zoo admission on Oct. 1, and Medicare Misty will host an informative presentation at 11 a.m. under Mimi's Pavilion.",
       "url": "https://www.chattzoo.org/events/senior-day",
+      "venue": "Chattanooga Zoo",
+      "category": "Family",
+      "source": "chattzoo.org"
+    },
+    {
+      "id": "af270d404219",
+      "title": "Boo in the Zoo",
+      "start": "2026-10-09T16:00:00",
+      "end": null,
+      "description": "Our annual Halloween celebration includes lots of candy, family-friendly decorations, costume shows, a bounce house, character meet and greets with Magic Lamp Entertainment and more! Enjoy an evening of costumes, critters and candy while you explore the Zoo!",
+      "url": "https://www.chattzoo.org/events/boo-in-the-zoo",
       "venue": "Chattanooga Zoo",
       "category": "Family",
       "source": "chattzoo.org"
