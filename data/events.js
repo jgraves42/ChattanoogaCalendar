29c89969891ea10c6501eb037aacefbede101ef2
jgraves42 <133706711+jgraves42@users.noreply.github.com>
2026-09-07 14:11:10 +0000

    chore: weekly event data update [skip ci]

--- data/events.js
+++ data/events.js
@@ -1,5 +1,5 @@
 window.CHATTANOOGA_EVENTS = {
-  "last_updated": "2026-08-31T16:05:35.252144+00:00",
+  "last_updated": "2026-09-07T14:11:10.459496+00:00",
   "events": [
     {
       "id": "bf4e3aae5ec6",
@@ -13,17 +13,6 @@ window.CHATTANOOGA_EVENTS = {
       "source": "tnaqua.org"
     },
     {
-      "id": "3bf7a883479a",
-      "title": "Senior Day at the Zoo",
-      "start": "2026-10-01T09:00:00",
-      "end": null,
-      "description": "Animal lovers who are 65+, this is your day! Seniors receive FREE Zoo admission on Oct. 1, and Medicare Misty will host an informative presentation at 11 a.m. under Mimi's Pavilion.",
-      "url": "https://www.chattzoo.org/events/senior-day",
-      "venue": "Chattanooga Zoo",
-      "category": "Family",
-      "source": "chattzoo.org"
-    },
-    {
       "id": "8a3127a14a3e",
       "title": "TN River Rescue Clean-Up",
       "start": "2026-10-03T00:00:00",
@@ -33,17 +22,6 @@ window.CHATTANOOGA_EVENTS = {
       "venue": "Tennessee Aquarium",
       "category": "Family",
       "source": "tnaqua.org"
-    },
-    {
-      "id": "af270d404219",
-      "title": "Boo in the Zoo",
-      "start": "2026-10-09T16:00:00",
-      "end": null,
-      "description": "Our annual Halloween celebration includes lots of candy, family-friendly decorations, costume shows, a bounce house, character meet and greets with Magic Lamp Entertainment and more! Enjoy an evening of costumes, critters and candy while you explore the Zoo!",
-      "url": "https://www.chattzoo.org/events/boo-in-the-zoo",
-      "venue": "Chattanooga Zoo",
-      "category": "Family",
-      "source": "chattzoo.org"
     }
   ]
 };
